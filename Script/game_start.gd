1c621deadcee018751aa91b027b5b2bf713a39c6
Midhun-krish-na <midhun200405@gmail.com>
2024-12-29 08:16:18 +0530

    1st commit.2

--- Script/game_start.gd
+++ Script/game_start.gd
@@ -4,11 +4,17 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"UI's/BoxContainer/HBoxContainer/VBoxContainer/MainMenu".connect("pressed",_on_pressed_menu)
+	#$UI/ID/MainMenu.connect("pressed",_on_pressed_menu)
+	#$UI/ID/Exit.connect("pressed",_on_pressed_Exit)
+	pass
 
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 func _on_pressed_menu():
-	get_tree().change_scene_to(MainMenu)
+	get_tree().change_scene_to_packed(MainMenu)
+
+func _on_pressed_Exit():
+	get_tree().quit()
--- Script/main_screen.gd
+++ Script/main_screen.gd
@@ -18,23 +18,23 @@ func _ready() -> void:
 	add_child(hover_sound)
 	add_child(click_sound)
 
-#Connect Button Signals
-	$ButtonContainer/StartButton.connect("pressed", _on_start_pressed)
-	$ButtonContainer/ContinueButton.connect("pressed", _on_continue_pressed)
-	$ButtonContainer/SettingButton.connect("pressed", _on_settings_pressed)
-	$ButtonContainer/CreditButton.connect("pressed", _on_credits_pressed)
-	$ButtonContainer/QuitButton.connect("pressed",_on_quit_pressed )
+#Connect Button Signals 
+	$ButtonContainer/Start.connect("pressed", _on_start_pressed)
+	$ButtonContainer/Continue.connect("pressed", _on_continue_pressed)
+	$ButtonContainer/Setting.connect("pressed", _on_settings_pressed)
+	$ButtonContainer/Credit.connect("pressed", _on_credits_pressed)
+	$ButtonContainer/Quit.connect("pressed",_on_quit_pressed )
 
 	#Conect Hover signals for all buttons
 	for button in $ButtonContainer.get_children():
 		button.connect("mouse_entered", _on_button_hover)
+
 
 func _on_start_pressed():
 	click_sound.play()
 	get_tree().change_scene_to_packed(game_sc)
 	pass
 	
-
 func _on_continue_pressed():
 	click_sound.play()
 	#Load / Save game logic
